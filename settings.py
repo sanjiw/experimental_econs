ddf92c2cb14ec6dd989f5fbad5aa7268179398b3
sanjiw <sanji.wibisana@gmail.com>
2020-08-02 12:46:00 +0700

    new

--- settings.py
+++ settings.py
@@ -89,8 +89,7 @@ ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 
 
 # Consider '', None, and '0' to be empty/false
-DEBUG = False
-#DEBUG = (environ.get('OTREE_PRODUCTION') in {None, '', '0'})
+DEBUG = (environ.get('OTREE_PRODUCTION') in {None, '', '0'})
 
 DEMO_PAGE_INTRO_HTML = """
 Here are various games implemented with 
