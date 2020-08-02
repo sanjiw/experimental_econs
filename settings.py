edf509db99039e5d3d5e0b14c49d8c56a6f3f292
sanjiw <sanji.wibisana@gmail.com>
2020-08-02 12:34:24 +0700

    environ

--- settings.py
+++ settings.py
@@ -89,7 +89,7 @@ ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 
 
 # Consider '', None, and '0' to be empty/false
-DEBUG = (environ.get('OTREE_PRODUCTION') in {None, '', '0'})
+DEBUG = False
 #DEBUG = (environ.get('OTREE_PRODUCTION') in {None, '', '0'})
 
 DEMO_PAGE_INTRO_HTML = """
